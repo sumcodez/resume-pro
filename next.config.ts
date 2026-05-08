a2670686875b623b042305458be1dfaa4425312d
sumcodez <cloud7suman@gmail.com>
2026-05-09 04:56:23 +0530

    pdf upload fix

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: [
     "@napi-rs/canvas",
-    "@napi-rs/canvas-win32-x64-msvc",
     "pdf-parse",
     "pdfjs-dist",
   ],
+  outputFileTracingIncludes: {
+    "/api/upload": [
+      "./node_modules/@napi-rs/canvas*/**/*",
+      "./node_modules/pdf-parse/dist/pdf-parse/cjs/pdf.worker.mjs",
+    ],
+  },
 };
 
 export default nextConfig;
